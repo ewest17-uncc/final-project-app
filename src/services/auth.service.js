f1c88a2370413d8d645f2fd5b0da688d63a724ed
Evan West <evan.west@cmrsurgical.com>
2020-12-16 14:43:57 +0000

    update api link

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://final-project-ewest17.herokuapp.com/api/auth/";
 
 class AuthService {
   login(username, password) {
